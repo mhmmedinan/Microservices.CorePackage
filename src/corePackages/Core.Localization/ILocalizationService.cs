ba74f32acd39a08bd4381f646fd5a8c3a2fe02df
mhmmedinan <minan.65@hotmail.com>
2025-04-16 00:39:59 +0300

    feat: implement Core.Localization package with JSON support and dynamic translation structure

---
+++ src/corePackages/Core.Localization/ILocalizationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Localization;
+
+public interface ILocalizationService
+{
+    public ICollection<string>? AcceptLocales { get; set; }
+    public Task<string> GetLocalizedAsync(string key, string? keySection = null);
+
+    public Task<string> GetLocalizedAsync(string key, ICollection<string> acceptLocales, string? keySection = null);
+}
---
+++ src/corePackages/Core.Localization/LocalizationMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Immutable;
+using Microsoft.Net.Http.Headers;
+
+namespace Core.Localization;
+
+public class LocalizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LocalizationMiddleware(RequestDelegate next)
+    {
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+    }
+
+    public async Task Invoke(HttpContext context, ILocalizationService localizationService)
+    {
+        IList<StringWithQualityHeaderValue> acceptLanguages = context.Request.GetTypedHeaders().AcceptLanguage;
+        if (acceptLanguages.Count > 0)
+            localizationService.AcceptLocales = acceptLanguages
+                .OrderByDescending(x => x.Quality ?? 1)
+                .Select(x => x.Value.ToString())
+                .ToImmutableArray();
+
+        await _next(context);
+    }
+}
